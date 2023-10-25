cf332ff5829a5030952c457efacdc8a3dcd7f562
truyduoitv0@gmail.com <truyduoitv0@gmail.com>
2023-10-25 17:13:13 +0700

    init project session 2

---
+++ web/dao.py
@@ -0,0 +1,39 @@
+def get_categories():
+    return [{
+        "id": 1,
+        "name": "Mobile"
+    }, {
+        "id": 2,
+        "name": "Table"
+    }]
+
+
+def get_products():
+   products = [{
+        "id": 1,
+        "name": "iphone13",
+        "price": 20000,
+        "image": "https://media.wired.com/photos/6148ef98a680b1f2086efee0/master/w_1920,c_limit/Gear-Review-Apple_iphone13_hero_us_09142021.jpg"
+    }, {
+        "id": 2,
+        "name": "Galaxys23",
+        "price": 20000,
+        "image": "https://media.wired.com/photos/6148ef98a680b1f2086efee0/master/w_1920,c_limit/Gear-Review-Apple_iphone13_hero_us_09142021.jpg"
+    }, {
+        "id": 3,
+        "name": "Galaxys23",
+        "price": 20000,
+        "image": "https://media.wired.com/photos/6148ef98a680b1f2086efee0/master/w_1920,c_limit/Gear-Review-Apple_iphone13_hero_us_09142021.jpg"
+    }, {
+        "id": 4,
+        "name": "iphone13",
+        "price": 20000,
+        "image": "https://media.wired.com/photos/6148ef98a680b1f2086efee0/master/w_1920,c_limit/Gear-Review-Apple_iphone13_hero_us_09142021.jpg"
+    }, {
+        "id": 5,
+        "name": "Galaxys23",
+        "price": 20000,
+        "image": "https://media.wired.com/photos/6148ef98a680b1f2086efee0/master/w_1920,c_limit/Gear-Review-Apple_iphone13_hero_us_09142021.jpg"
+    }
+   ]
+   return products
